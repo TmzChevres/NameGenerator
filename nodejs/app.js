e79995f8a14364571bd6a70f5b51e4fa7c5e5cbe
Tomaz Chevres <tchevres-24@peddie.org>
2023-07-16 22:49:56 -0400

    test secure.json

--- nodejs/app.js
+++ nodejs/app.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const port = 5623;
 
+// Read in the contents of the secure.json file
+const secureData = fs.readFileSync('secure.json');
+const secure = JSON.parse(secureData);
+
 
 // sets express to listen on local port
 app.listen(port, () => {
@@ -13,5 +17,5 @@ app.listen(port, () => {
 // test to make sure it is working
 // connect with /nodejs/test
 app.get('/test', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World! ' + secure.APIKey);
 });
