89290510dcc7dc07da31f68a72ccfd4e966ab569
Tomaz Chevres <tchevres-24@peddie.org>
2023-07-16 23:20:43 -0400

    test openai api

--- nodejs/app.js
+++ nodejs/app.js
@@ -29,7 +29,7 @@ app.get('/test', (req, res) => {
 // const openaiAPIKey = secure.APIKey;
 // const chatgpt = new openai(openaiAPIKey);
 
-import { Configuration, OpenAIApi } from "openai";
+const { Configuration, OpenAIApi } = require('openai');;
 const configuration = new Configuration({
     organization: "org-vcr7rrSPfrXSfF25zYsJuF5N",
     apiKey: secure.APIKey,
