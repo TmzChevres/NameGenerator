11510d31bdbede23b3bc305ca6d2efe80fdc0c69
Tomaz Chevres <tchevres-24@peddie.org>
2023-07-16 22:59:01 -0400

    test openai api

--- nodejs/app.js
+++ nodejs/app.js
@@ -33,21 +33,21 @@ const chatgpt = new openai(openaiAPIKey);
 // Define the parameters for your chat conversation
 const params = {
     messages: [
-      { role: 'system', content: 'You are a helpful assistant.' },
-      { role: 'user', content: 'Who won the world series in 2020?' },
-      { role: 'assistant', content: 'The Los Angeles Dodgers won the World Series in 2020.' },
-      { role: 'user', content: 'Where was it played?' },
-      { role: 'assistant', content: 'The games were played in Arlington, Texas.' }
+        { role: 'system', content: 'You are a helpful assistant.' },
+        { role: 'user', content: 'Who won the world series in 2020?' },
+        { role: 'assistant', content: 'The Los Angeles Dodgers won the World Series in 2020.' },
+        { role: 'user', content: 'Where was it played?' },
+        { role: 'assistant', content: 'The games were played in Arlington, Texas.' }
     ]
-  };
-  
-  // Make the API call
-  chatgpt.complete(params)
+};
+
+// Make the API call
+chatgpt.complete(params)
     .then(response => {
-      const { choices } = response.data;
-      const reply = choices[0].message.content;
-      console.log('ChatGPT Reply:', reply);
+        const { choices } = response.data;
+        const reply = choices[0].message.content;
+        console.log('ChatGPT Reply:', reply);
     })
     .catch(error => {
-      console.error('Error:', error);
+        console.error('Error:', error);
     });
