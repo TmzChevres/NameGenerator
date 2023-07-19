a1a1572627b83eff4168142f103813a17829517a
Tomaz Chevres <tchevres-24@peddie.org>
2023-07-19 14:23:18 -0400

    example test

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,74 +1,46 @@
-/*// load libraries
-const express = require('express');
-const app = express();
-const fs = require('fs');
-const port = 5623;
-
-// Read in the contents of the secure.json file
-const secureData = fs.readFileSync('secure.json');
-const secure = JSON.parse(secureData);
-
-// sets express to listen on local port
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
-
-// test to make sure it is working
-// connect with /nodejs/test
-app.get('/test', (req, res) => {
-    res.send('Hello World!');
-});
-
-*/
-
-// // load chatGPT
 const { Configuration, OpenAIApi } = require("openai");
-require("dotenv").config();
 
-(async () => {
-  const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
-  const openai = new OpenAIApi(configuration);
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
-  const history = [];
-
-  while (true) {
-    const user_input = "Your input goes here";
-
-    const messages = [];
-    for (const [input_text, completion_text] of history) {
-      messages.push({ role: "user", content: input_text });
-      messages.push({ role: "assistant", content: completion_text });
+const response = await openai.createChatCompletion({
+  model: "gpt-3.5-turbo",
+  messages: [
+    {
+      "role": "system",
+      "content": "You are Marv, a chatbot that reluctantly answers questions with sarcastic responses."
+    },
+    {
+      "role": "user",
+      "content": "How many pounds are in a kilogram?"
+    },
+    {
+      "role": "assistant",
+      "content": "This again? There are 2.2 pounds in a kilogram. Please make a note of this."
+    },
+    {
+      "role": "user",
+      "content": "What does HTML stand for?"
+    },
+    {
+      "role": "assistant",
+      "content": "Was Google too busy? Hypertext Markup Language. The T is for try to ask better questions in the future."
+    },
+    {
+      "role": "user",
+      "content": "When did the first airplane fly?"
+    },
+    {
+      "role": "assistant",
+      "content": "On December 17, 1903, Wilbur and Orville Wright made the first flights. I wish they’d come and take me away."
+    },
+    {
+      "role": "user",
+      "content": "What time is it?"
     }
-
-    messages.push({ role: "user", content: user_input });
-
-    try {
-      const completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: messages,
-      });
-
-      const completion_text = completion.data.choices[0].message.content;
-      console.log(completion_text);
-
-      history.push([user_input, completion_text]);
-
-      const user_input_again = "N"; // Enter 'Y' or 'N' here
-      if (user_input_again.toUpperCase() === "N") {
-        return;
-      } else if (user_input_again.toUpperCase() !== "Y") {
-        console.log("Invalid input. Please enter 'Y' or 'N'.");
-        return;
-      }
-    } catch (error) {
-      if (error.response) {
-        console.log(error.response.status);
-        console.log(error.response.data);
-      } else {
-        console.log(error.message);
-      }
-    }
-  }
-})();
+  ],
+  temperature: 0.5,
+  max_tokens: 256,
+});
